cff3f28dd1f2b871f8494880e2122794cfbfb81f
JFortun <josemanuelfortunromero@gmail.com>
2021-08-03 12:36:44 +0200

    Added counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title: string = 'Counter App';
+  title: string = 'Counter App';
+  number: number = 10;
+  base: number = 5;
+
+  sum(value: number) {
+    this.number += value;
+  }
+
+  subtract(value: number) {
+    this.number -= value;
+  }
 }
