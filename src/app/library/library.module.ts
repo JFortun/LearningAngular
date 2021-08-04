2df5a370c0b660dd4e608be979db27181e1e6296
JFortun <josemanuelfortunromero@gmail.com>
2021-08-04 09:50:57 +0200

    Library form with ngForm

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MainPageComponent } from './main-page/main-page.component';
-
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MainPageComponent} from './main-page/main-page.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +11,9 @@ import { MainPageComponent } from './main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
-export class LibraryModule { }
+export class LibraryModule {
+}
