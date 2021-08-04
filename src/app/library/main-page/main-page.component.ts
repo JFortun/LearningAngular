fd4aa3fb1a57fb1c8daff36cf99082a5153aa04f
JFortun <josemanuelfortunromero@gmail.com>
2021-08-04 10:25:11 +0200

    Update book list with form

--- src/app/library/main-page/main-page.component.ts
+++ src/app/library/main-page/main-page.component.ts
@@ -1,12 +1,37 @@
 import {Component} from '@angular/core';
 
+interface Book {
+  name: string;
+  pages: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  books: Book[] = [
+    {
+      name: 'Book 1',
+      pages: 135
+    },
+    {
+      name: 'Book 2',
+      pages: 420
+    }
+  ]
+
+  new: Book = {
+    name: '',
+    pages: 0
+  }
+
   add() {
-    console.log('Works');
+    if (this.new.name.trim().length == 0) {
+      return;
+    }
+    this.books.push(this.new);
+    this.new = {name: '', pages: 0};
   }
 }
